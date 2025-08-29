362720480fafcccb9ccc263efd87a70c575bdb7e
Vincent-SChevrier <1668852@cstjean.qc.ca>
2025-08-29 17:56:20 -0400

    Dossier devoir3 programmation systeme

---
+++ Install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Partie 1 - Install.sh"
+
+echo `mkdir bin`
+echo `mkdir cegep`
+echo `mkdir projets`
+echo `sudo pacman -Syu`
+echo `sudo pacman -S git`
+echo `git --version`
+echo `sudo pacman -S neovim`
+echo `neovim --version`
+echo `sudo pacman -S basedevel`
+echo `basedevel --version`
+echo `sudo pacman -S gdb`
+echo `gdb --version`
+echo `sudo pacman -S python3`
+echo `sudo pacman -S python-pip`
+echo `python3 --version`
+
+echo "Partie 2 - Dotfiles"
+echo "Cours3 - 2.Personnalisation"
+
